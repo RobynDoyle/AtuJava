5219d1356edfe35cd4156f27888c7002ede42b8c
Robyn Doyle <robyn.doyle@gmail.com>
2024-12-10 18:24:54 +0100

    Added wage program

---
+++ week2/wage.java
@@ -0,0 +1,24 @@
+package week2;
+
+import java.util.Scanner;
+
+public class wage {
+
+    public static void main(String[] args){
+        
+        Scanner keyboardIn = new Scanner(System.in);
+        
+        double wage = 15;
+        System.out.print("Number of hours worked in a week?: ");
+        double hours = keyboardIn.nextDouble();
+        double salary = wage * hours;
+
+        
+
+        System.out.println("Your total salary is = €" + salary);
+
+    
+    
+    }
+    
+}
