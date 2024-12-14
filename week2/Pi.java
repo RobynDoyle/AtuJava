0d65a6f2c5c36a55235df60c199dad3ed4b60ac0
Robyn Doyle <robyn.doyle@gmail.com>
2024-12-14 19:54:40 +0100

    Added Pi

---
+++ week2/Pi.java
@@ -0,0 +1,20 @@
+package week2;
+import java.util.Scanner;
+
+public class Pi {
+
+    public static void main(String[] args){
+
+    Scanner keyboardIn = new Scanner(System.in);
+    final double  Pi = 3.142;
+
+    System.out.print("Enter the radius: ");
+    double R = keyboardIn.nextDouble();
+    double Area = Pi * R * R;
+    System.out.println("Area is equal to " + Area + "cm2");
+
+    }
+
+
+    
+}
