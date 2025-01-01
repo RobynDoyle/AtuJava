9e6bb0029be403ed279e9aaeabdfc0b4a1a3892b
Robyn Doyle <robyn.doyle@gmail.com>
2025-01-01 15:43:38 +0100

    moon

---
+++ week4/Moon.java
@@ -0,0 +1,16 @@
+package week4;
+import java.util.Scanner;
+
+public class Moon {
+    public static void main(String[] args){
+        System.out.print("The number of years that I wish to calculate moons for is: ");
+        Scanner keyboardIn = new Scanner(System.in);
+        double years = keyboardIn.nextDouble();
+        double total_moons = years * 12.37;
+        
+        System.out.println("Total moons in " + years + " years was " + total_moons);
+
+
+    }
+    
+}
