b4a240080b47babbfb802dc182d23eb8d650d784
Robyn Doyle <robyn.doyle@gmail.com>
2025-01-17 19:55:11 +0100

    Added exam file

---
+++ week4/DoWhile.java
@@ -0,0 +1,14 @@
+package week4;
+
+public class DoWhile {
+
+    public static void main(String[] args){
+        int temp = 10;
+        do 
+        {
+            System.out.println("It is " + temp + " degrees.");
+            temp--;
+        } while (temp > 0);
+    }
+    
+}
