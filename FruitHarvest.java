bb7b9aae08bef4d36771511e4723b98717be8d57
Robyn Doyle <robyn.doyle@gmail.com>
2025-01-21 11:59:02 +0000

    Finalized harvest picker

--- FruitHarvest.java
+++ FruitHarvest.java
@@ -2,7 +2,8 @@
  * Author: Robyn Doyle 
  * Date: January 20, 2025
  * Description: This program asks the user to input the number of baskets collected.
- * It then calculates the pay due to the Picker. Then it asks if user wants to input info for another Picker.
+ * (Decision was made to not use a switch to input basket info, since this would make the input process tedious and long)
+ * Program then calculates the pay due to the Picker. Then it asks if user wants to input info for another Picker.
  * Once all Pickers info has been entered, the program outputs the days highest earning Picker and stats. 
  */
 
@@ -19,74 +20,106 @@ public class FruitHarvest {
         //Set up variables
         char continueAnotherPicker = 'y'; // Setting this to 'y' will allow initial entry into the while loop
         Integer numberOfPickers = 0; // Counter to check how many Pickers were processed in this cycle. 
+        Integer smallBasketCounter, mediumBasketCounter, largeBasketCounter, overallSmallBasketCounter = 0, overallMediumBasketCounter = 0, overallLargeBasketCounter = 0, overallTotalBasketCounter = 0;
+        double earningsSmall, earningsMedium, earningsLarge, totalEarningsWithoutBonus, bonus, totalEarningsWithBonus, overallTotalPayment = 0; //Cash variables with decimal precision. 
+        Integer highestPicker = 1; // Tracks the picker with the highest payment. 
+        double highestPickerAmount = 0; // Counts the earnings of todays highest paid picker. 
+
+
+        System.out.println();  
+        System.out.println("***************************************************************************");
+
         
-        Integer smallbasketNumber, mediumbasketNumber, largebasketNumber, overallSmallbasketNumber = 0, overallMediumbasketNumber = 0, overallLargebasketNumber = 0, overallTotalBasketNumber = 0;
-        double earningsSmall, earningsMedium, earningsLarge, totalCollectionCalculation1, bonus, totalCollectionCalculation2, overallTotalPayment = 0;
-        double highestPickerAmount = 0;
-        Integer highestPicker = 1;
-        
-        
-        while (continueAnotherPicker == 'y') {
+        // While loop that runs when sentinel variable is set to 'y' or 'Y'. This runs until user escapes the loop with 'n'. 
+        while (continueAnotherPicker == 'y' || continueAnotherPicker == 'Y') {
             System.err.println();
             numberOfPickers++;
-            System.out.println("Picker #" + numberOfPickers + ":");
+            System.out.println("Picker #" + numberOfPickers + ": Input");
             System.out.println("\tEnter the number of baskets of fruit you collected per basket size");
             
-            // Taking input for baskets quantity
-            System.out.print("\tSmall Basket Quantity:");
-            smallbasketNumber = keyboardIn.nextInt();
-            overallSmallbasketNumber = overallSmallbasketNumber + smallbasketNumber;
-            System.out.print("\tMedium Basket Quantity:");
-            mediumbasketNumber = keyboardIn.nextInt();
-            overallMediumbasketNumber = overallMediumbasketNumber + mediumbasketNumber;
-            System.out.print("\tLarge Basket Quantity:");
-            largebasketNumber = keyboardIn.nextInt();
-            overallLargebasketNumber = overallLargebasketNumber + largebasketNumber;
+            // Taking input for Small baskets quantity. Wont accept negative numbers. 
+            do {
+                System.out.print("\tSmall Basket Quantity: ");
+                smallBasketCounter = keyboardIn.nextInt();
+                if (smallBasketCounter < 0) {
+                    System.out.println("Invalid input. Basket quantity needs to be positive number");
+                }
+            } while (smallBasketCounter < 0);
+
+            // Adds number of small baskets entered to an overall small basket counter
+            overallSmallBasketCounter = overallSmallBasketCounter + smallBasketCounter;
+
+            // Taking input for Medium baskets quantity. Wont accept negative numbers. 
+            do {
+                System.out.print("\tMedium Basket Quantity: ");
+                mediumBasketCounter = keyboardIn.nextInt();
+                if (mediumBasketCounter < 0) {
+                    System.out.println("Invalid input. Basket quantity needs to be positive number");
+                }
+            } while (mediumBasketCounter < 0);
+
+            // Adds number of Medium baskets entered to an overall Medium basket counter
+            overallMediumBasketCounter = overallMediumBasketCounter + mediumBasketCounter;
+            
+            // Taking input for Large baskets quantity. Wont accept negative numbers. 
+            do {
+                System.out.print("\tLarge Basket Quantity: ");
+                largeBasketCounter = keyboardIn.nextInt();
+                if (largeBasketCounter < 0) {
+                    System.out.println("Invalid input. Basket quantity needs to be positive number");
+                }
+            } while (largeBasketCounter < 0);
+            
+            // Adds number of Large baskets entered to an overall Large basket counter
+            overallLargeBasketCounter = overallLargeBasketCounter + largeBasketCounter;
+            
             System.out.println();
             System.out.println("***************************************************************************");
 
 
             // Calculate earnings per basket type
-            earningsSmall = (double)smallbasketNumber * 6;
-            earningsMedium = (double)mediumbasketNumber * 8;
-            earningsLarge = (double)largebasketNumber * 12;
-            totalCollectionCalculation1 = earningsSmall + earningsMedium + earningsLarge; 
+            earningsSmall = (double)smallBasketCounter * 6;
+            earningsMedium = (double)mediumBasketCounter * 8;
+            earningsLarge = (double)largeBasketCounter * 12;
+            totalEarningsWithoutBonus = earningsSmall + earningsMedium + earningsLarge; 
 
-            if (totalCollectionCalculation1 > 30 && totalCollectionCalculation1 < 40){
-                bonus = totalCollectionCalculation1 * .15;
-                totalCollectionCalculation2 = totalCollectionCalculation1 + bonus;
+            // Calculates 15% Bonus, if bonus threshold 1 is reached.
+            if (totalEarningsWithoutBonus > 30 && totalEarningsWithoutBonus < 40){
+                bonus = totalEarningsWithoutBonus * .15;
+                totalEarningsWithBonus = totalEarningsWithoutBonus + bonus;
+            } // Calculates 25% Bonus, if bonus threshold 2 is reached.
+            else if (totalEarningsWithoutBonus > 40){
+                bonus = totalEarningsWithoutBonus * .25;
+                totalEarningsWithBonus = totalEarningsWithoutBonus + bonus;
             }
-            else if (totalCollectionCalculation1 > 40){
-                bonus = totalCollectionCalculation1 * .25;
-                totalCollectionCalculation2 = totalCollectionCalculation1 + bonus;
-            }
-            // Setting the final total to be equal to a total without a bonus.
+            // Setting the final total to be equal to a total without a bonus if no threshold is reached.
             else {
-                totalCollectionCalculation2 = totalCollectionCalculation1;
+                totalEarningsWithBonus = totalEarningsWithoutBonus;
             }
             
-            if (totalCollectionCalculation2 > highestPickerAmount){
-                highestPickerAmount = totalCollectionCalculation2;
-                // sets the highest picker container to match the current pickers number. 
+            // sets the highest picker container to match the current pickers number. Tracks who the highest Pickers is. 
+            if (totalEarningsWithBonus > highestPickerAmount){
+                highestPickerAmount = totalEarningsWithBonus;
                 highestPicker = numberOfPickers; 
-
             }
 
-            overallTotalPayment = overallTotalPayment + totalCollectionCalculation2;
+            // Tracks the overall total to be paid by farmer. 
+            overallTotalPayment = overallTotalPayment + totalEarningsWithBonus;
+
+            // Outputs Summary data.
             System.out.println(); System.out.println(); 
+            System.out.println("Picker #" + numberOfPickers + ": Review");
+            System.out.println("\tNumber of baskets collect = " + (smallBasketCounter + mediumBasketCounter + largeBasketCounter));  
+            System.out.println(String.format("\tEarnings for Small Baskets: $%.2f", earningsSmall));
+            System.out.println(String.format("\tEarnings for Medium Baskets: $%.2f", earningsMedium));
+            System.out.println(String.format("\tEarnings for Large Baskets = $%.2f", earningsLarge));
+            System.out.println(String.format("\tTotal Basket earnings without any bonus = $%.2f", totalEarningsWithoutBonus));
 
-            System.out.println("Picker #" + numberOfPickers);
-            System.out.println("\tNumber of baskets collect = " + (smallbasketNumber + mediumbasketNumber + largebasketNumber));
-             
-            System.out.println("\tEarnings for Small Baskets = $" + earningsSmall);
-            System.out.println("\tEarnings for Medium Baskets = $" + earningsMedium);
-            System.out.println("\tEarnings for Large Baskets = $" + earningsLarge);
-            System.out.println("\tTotal Basket earnings without any bonus = $" + totalCollectionCalculation1);
-
-            if (totalCollectionCalculation1 != totalCollectionCalculation2){
-                System.out.println("\tTotal Basket earnings with added bonus = $" + totalCollectionCalculation2);
+            // Only outputs bonus value, if a bonus has indeed been allocated. 
+            if (totalEarningsWithoutBonus != totalEarningsWithBonus){
+                System.out.println(String.format("\tTotal Basket earnings with added bonus = $%.2f", totalEarningsWithBonus));
             }
-
+            
             System.out.println();
             System.out.println("***************************************************************************\n");
 
@@ -104,21 +137,23 @@ public class FruitHarvest {
             System.out.println("***************************************************************************\n");
 
         }
-    overallTotalBasketNumber = overallSmallbasketNumber + overallMediumbasketNumber + overallLargebasketNumber;
+    // Caclulates total amount of baskets.     
+    overallTotalBasketCounter = overallSmallBasketCounter + overallMediumBasketCounter + overallLargeBasketCounter;
     
+    // Outputs Summary data.
     System.err.println("Total number of fruit picker: " + numberOfPickers);
 
-    System.out.println("Total baskets collected: " + overallTotalBasketNumber);   
-    System.err.println("Total small baskets: " + overallSmallbasketNumber);    
-    System.err.println("Total medium baskets: " + overallMediumbasketNumber);   
-    System.out.println("Total large baskets: " + overallLargebasketNumber); 
+    System.out.println("Total baskets collected: " + overallTotalBasketCounter);   
+    System.err.println("Total small baskets: " + overallSmallBasketCounter);    
+    System.err.println("Total medium baskets: " + overallMediumBasketCounter);   
+    System.out.println("Total large baskets: " + overallLargeBasketCounter); 
 
-    System.out.println("Total paid out: " + overallTotalPayment);
-    System.out.println("Todays Top Picker is Picker no. " + highestPicker + " with an amount of " + highestPickerAmount); 
+    System.out.println(String.format("Total paid out: $%.2f", overallTotalPayment));
+    System.out.println(String.format("Todays Top Picker is Picker no. " + highestPicker + " with an amount of $%.2f", highestPickerAmount)); 
     System.out.println();  
     System.out.println("***************************************************************************");
 
-    // Close the Scanner
+    // Closes the user input Scanner.
     keyboardIn.close();
 
 
