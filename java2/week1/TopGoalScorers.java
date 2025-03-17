99afa3bc00bc7b9691223a46e842a9f3423354ae
Robyn Doyle <robyn.doyle@gmail.com>
2025-03-17 19:03:31 +0100

    Added playername array list

--- java2/week1/TopGoalScorers.java
+++ java2/week1/TopGoalScorers.java
@@ -1,18 +1,22 @@
 package java2.week1;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TopGoalScorers {
     public static void main(String[] args) {
+        Scanner keyboardIn = new Scanner(System.in);
         ArrayList <String> goalScorers = new ArrayList<>();
         String gsOne = "Mark";
         String gsTwo = "Bob";
         String gsThree = "Tom";
 
+        int size = 5;
 
-        goalScorers.add(gsOne);
-        goalScorers.add(gsTwo);
-        goalScorers.add(gsThree);
-
+        for (int i = 0; i < size; i++){
+            System.out.println("Enter the name of player " + (i + 1) + ": ");
+            String playerName = keyboardIn.nextLine();
+            goalScorers.add(playerName);
+        }
         
         for(int i = 0; i < goalScorers.size(); i++){
             System.out.println(goalScorers.get(i));
