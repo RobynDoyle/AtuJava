082cca2b8f79a6d7de774ab060fa9661822c205d
Robyn Doyle <robyn.doyle@gmail.com>
2025-03-17 19:14:40 +0100

    Fixed code

--- java2/week1/TopGoalScorers.java
+++ java2/week1/TopGoalScorers.java
@@ -21,5 +21,25 @@ public class TopGoalScorers {
         for(int i = 0; i < goalScorers.size(); i++){
             System.out.println(goalScorers.get(i));
         }
+
+        System.out.println("Do you wish to overwrite one of the names?: y/n");
+        String choice = keyboardIn.nextLine();
+
+        if(choice.equals("y")){
+            System.out.println("Enter the position and then the name of the player");
+            System.err.println("Position in array: ");
+            int position = keyboardIn.nextInt();
+            keyboardIn.nextLine(); // Consume newline
+            System.out.println("New Name: ");
+            String newName = keyboardIn.nextLine();
+
+            goalScorers.set(position - 1, newName);
+
+        }
+
+        for(int i = 0; i < goalScorers.size(); i++){
+            System.out.println(goalScorers.get(i));
+        }
+
     }
 }
